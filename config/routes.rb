f6ab87acf09c0f9b858555c7f3761c7a8b007d33
Cedric ZH <cedriczhchan@gmail.com>
2016-09-07 17:16:15 +0800

    Remove subdomain to test on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   resources :users
-  constraints subdomain: 'api' do
-    scope module: 'api' do
-      namespace :v1 do
-        resources :users
-      end
+  namespace :api do
+    namespace :v1 do
+      resources :users
     end
   end
 end
