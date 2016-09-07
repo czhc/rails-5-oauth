84743ec5afb9d853605311555cd91137cb485e96
Cedric ZH <cedriczhchan@gmail.com>
2016-09-07 16:40:17 +0800

    Initial commit with basic User scaffold and custom rack middleware

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :users
+  constraints subdomain: 'api' do
+    scope module: 'api' do
+      namespace :v1 do
+        resources :users
+      end
+    end
+  end
+end
