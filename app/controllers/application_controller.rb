0eac046d5194dbcf7562cedec93f8a81122d2abf
Cedric ZH <cedriczhchan@gmail.com>
2016-09-08 12:27:42 +0800

    Add CORS callbacks to secure requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
   include ActionController::Serialization
   before_action :authenticate
+  before_action :cors_preflight_check
+  after_action :cors_set_access_control_headers
 
   protected
 
@@ -14,11 +16,24 @@ class ApplicationController < ActionController::API
     authenticate_with_http_token do |token, options|
       @current_user = User.find_by(api_access_token: token)
       session[:user_id] = @current_user&.id
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render text: '', content_type: 'text/plain'
     end
   end
 
   def current_user
     return @current_user
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
   end
   def render_unauthorized(realm = "Application")
     self.headers["WWW-Authenticate"] = %(Token realm="#{realm.gsub(/"/, "")}")
