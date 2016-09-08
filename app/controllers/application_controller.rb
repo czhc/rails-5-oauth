54367d22abc338bbbaeec9258ba00e031d203777
Cedric ZH <cedriczhchan@gmail.com>
2016-09-08 13:45:35 +0800

    Rewrite authorisation actions for doorkeeper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,46 @@
 class ApplicationController < ActionController::API
-  include ActionController::HttpAuthentication::Token::ControllerMethods
+  # include ActionController::HttpAuthentication::Token::ControllerMethods
   include ActionController::Serialization
-  before_action :authenticate
+  # before_action :authenticate
   before_action :cors_preflight_check
   after_action :cors_set_access_control_headers
 
   protected
 
-  # Authenticate the user with token based authentication
-  def authenticate
-    authenticate_token || render_unauthorized
+  # def authenticate
+  #   # authenticate_with_http_token do |token, options|
+  #     Rails.logger.debug "DEBUG http token: #{token.inspect}"
+
+  #     if doorkeeper_token
+  #       Rails.logger.debug "DEBUG doorkeeper_token: #{doorkeeper_token.inspect}"
+  #       @current_user = User.find(doorkeeper_token.resource_owner_id)
+  #       session[:user_id] = @current_user&.id
+  #     end
+  #   # end
+
+  #   return if @current_user
+
+  #   return render_unauthorized
+
+  #   # authenticate_with_http_token do |token, options|
+  #   #   @current_user = User.find_by(api_access_token: token)
+  #   #   session[:user_id] = @current_user&.id
+  #   # end
+  # end
+
+  def current_user
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
 
-  def authenticate_token
-    authenticate_with_http_token do |token, options|
-      @current_user = User.find_by(api_access_token: token)
-      session[:user_id] = @current_user&.id
+  # def render_unauthorized(realm = "Application")
+  #   self.headers["WWW-Authenticate"] = %(Token realm="#{realm.gsub(/"/, "")}")
+  #   render json: { errors: ['User not authenticated']}, status: :unauthorized
+  # end
+
+  # def errors_json(messages)
+  #   { errors: [*messages] }
+  # end
+
   def cors_preflight_check
     if request.method == 'OPTIONS'
       headers['Access-Control-Allow-Origin'] = '*'
@@ -27,16 +52,17 @@ class ApplicationController < ActionController::API
     end
   end
 
-  def current_user
-    return @current_user
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
     headers['Access-Control-Max-Age'] = "1728000"
   end
-  def render_unauthorized(realm = "Application")
-    self.headers["WWW-Authenticate"] = %(Token realm="#{realm.gsub(/"/, "")}")
-    render json: 'Bad credentials', status: :unauthorized
+
+  private
+
+  def current_resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
   end
+
 end
