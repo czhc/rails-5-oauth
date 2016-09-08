af68153d0f2fb731335b85cfd134ae2f38aefecf
Cedric ZH <cedriczhchan@gmail.com>
2016-09-08 14:06:43 +0800

    Allow user#create on api endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  # skip_before_action :doorkeeper_authorize!#, only: [:create]
+  skip_before_action :doorkeeper_authorize!#, only: [:create]
   # skip_before_action :authenticate, only: [:create]
   # skip_authorization_check
 
@@ -11,9 +11,9 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user
+      render json: @user, status: :created, location: @user
     else
-      render json: { error: 'Bad arguments'}, status: 403
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
 
@@ -22,6 +22,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:email, :password)
   end
